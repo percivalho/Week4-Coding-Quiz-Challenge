f7b48d0cc5be30cffdaf96419040ad5c69a8bc75
percivalho <35653644+percivalho@users.noreply.github.com>
2023-04-22 10:46:27 +0100

    Basic Functions Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,43 +5,44 @@ var questionSection = document.querySelector(".question");
 var finishSection = document.querySelector(".finish");
 var questionTitle = document.getElementById("questiontitle");
 var resultOutput = document.getElementById("result");
+var scoreOutput = document.getElementById("score");
+
+var currentQuestionIndex = 0;
+var timeLeft = 10;
+var score = 0;
+// make it global for stop the timer later
+var timeInterval;
 
 function countdown() {
-    var timeLeft = 76;
   
     // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
 
       if (timeLeft > 0){
         timeLeft--;
         timerEl.textContent = timeLeft;
       }
       if(timeLeft === 0) {
-        timerEl.textContent = "";
+        timerEl.textContent = "0";
         // Stops execution of action at set interval
-        clearInterval(countdown);
+        clearInterval(timeInterval);
         // Calls function to create and append image
         //displayMessage();
+        endQuiz();
   
       }
   
     }, 1000);
   }
 
-
-// Listen for a click event on toggle switch
-startSection.addEventListener("click", function() {
-    // call the function
-    countdown();
-
-    // set the questions out:
-    var question = questions[0];
+function displayQuestion(){
+    var question = questions[currentQuestionIndex];
     questionTitle.textContent = question.question;
 	const choices = document.getElementById("choices");
 	for (let i = 0; i < choices.children.length; i++) {
 		const choice = choices.children[i].querySelector(".choice");
 		choice.textContent = question.choices[i];
-		//choice.addEventListener("click", handleChoice);
+		choice.addEventListener("click", handleChoice);
 	}    
     //console.log(resultOutput)
     //resultOutput.textContent = "                        ";
@@ -52,4 +53,68 @@ startSection.addEventListener("click", function() {
     finishSection.setAttribute("style", "display:none;");
     //console.log(finish);
 
+}
+
+
+
+// Listen for a click event on toggle switch
+startSection.addEventListener("click", function() {
+    // call the function
+    countdown();
+
+    // set the questions out:
+    displayQuestion();
+
 });
+
+
+// Function to handle a choice click
+function handleChoice(event) {
+	// Get chosen answer
+	var chosenAnswer = event.target.textContent;
+
+    var question = questions[currentQuestionIndex];
+	// Check if answer is correct
+	if (chosenAnswer === question.answer) {
+        score += 1;
+		resultOutput.textContent = "Correct!";
+	} else {
+		// Subtract time and display feedback
+		timeLeft -= 5;
+		resultOutput.textContent = "Wrong!";
+	}
+
+	// Remove event listeners from choices
+	var choices = document.getElementById("choices");
+	for (let i = 0; i < choices.children.length; i++) {
+		const choice = choices.children[i].querySelector(".choice");
+		choice.removeEventListener("click", handleChoice);
+	}
+
+	// Check if there are more questions
+	currentQuestionIndex++;
+	if (currentQuestionIndex < questions.length) {
+		// Display next question
+		displayQuestion();
+	} else {
+		// Quiz is over
+		endQuiz();
+	}
+}
+
+// Function to end the quiz
+function endQuiz() {
+	// Stop the timer
+    clearInterval(timeInterval);
+
+    // display the score out
+    scoreOutput.textContent = score;
+
+    // display the questions out
+    startSection.setAttribute("style", "display:none;");
+    questionSection.setAttribute("style", "display:none");
+    finishSection.setAttribute("style", "display:inline;");
+
+	// Add event listener to submit button
+	//document.getElementById("submit").addEventListener("click", submitScore);
+}
